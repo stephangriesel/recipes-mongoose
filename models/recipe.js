55f3db64ed9072228280df78c79f4f45ce51a06a
stephangriesel <sgriesel@gmail.com>
2019-02-25 10:05:08 +0100

    done - not complete

---
+++ models/recipe.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// Set up schema
+var recipeSchema = new Schema({
+    title: String,
+    level: String,
+    ingredients: Array,
+    cuisine: String,
+    dishType: String,
+    image: String,
+    duration: Number,
+    creator: String,
+    created: Date
+  });
+
+  module.exports = mongoose.model('Recipe', recipeSchema);
